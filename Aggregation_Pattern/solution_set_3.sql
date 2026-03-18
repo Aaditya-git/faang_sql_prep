b708d66b6919cd0b94d4d3f11582d5eb091a1265
Aaditya Bhilegaonkar <aadityab134@gmail.com>
2026-03-17 19:45:48 -0700

    SET 3 MEDIUM DONE

--- Aggregation_Pattern/solution_set_3.sql
+++ Aggregation_Pattern/solution_set_3.sql
@@ -20,11 +20,61 @@ FROM
 -- b) Medium
 
 -- 3. Find the user who spent the most money in a single day.
+WITH SM AS(
+SELECT
+	O.USER_ID,
+	O.ORDER_DATE,
+    P.PRICE * O.QUANTITY  AS SM
+FROM 
+	ORDERS O JOIN PRODUCTS P ON
+    P.PRODUCT_ID = O.PRODUCT_ID),
+    RNK AS (
+    SELECT 
+		*,
+        DENSE_RANK() OVER(ORDER BY SM DESC) AS RN
+	FROM 
+		SM)
+        SELECT 
+			USER_ID FROM RNK WHERE RN = 1;
+    
 -- 4. Find the average revenue per order for each category.
+
+
+
 -- 5. Find the top 2 users by total spending.
+WITH JOINED_DATA AS (
+SELECT
+	O.USER_ID,
+    SUM(P.PRICE * O.QUANTITY) AS TOTAL_SPEND
+FROM
+	PRODUCTS P JOIN ORDERS O ON
+    P.PRODUCT_ID = O.PRODUCT_ID
+    GROUP BY O.USER_ID),
+     FINAL AS(
+SELECT 
+	USER_ID,
+    TOTAL_SPEND,
+    DENSE_RANK() OVER(ORDER BY TOTAL_SPEND DESC) AS D_RNK
+FROM 
+	JOINED_DATA)
+    SELECT USER_ID FROM FINAL WHERE D_RNK <=2;
 -- 6. Find the number of users who have never placed an order.
+SELECT 
+	U.USER_ID
+FROM USERS U LEFT JOIN ORDERS O ON 
+	U.USER_ID = O.USER_ID
+    WHERE O.ORDER_ID IS NULL;
 -- 7. Find the category with the lowest total revenue.
+SELECT
+	P.CATEGORY,
+	SUM(P.PRICE * O.QUANTITY) AS SM
+FROM 
+	PRODUCTS P JOIN ORDERS O ON
+    P.PRODUCT_ID = O.PRODUCT_ID
+    GROUP BY P.CATEGORY
+    ORDER BY SM LIMIT 1;
 -- 8. Find the users whose average order value is greater than 500.
+
 -- 9. Find the percentage contribution of each category to total revenue.
 
 -- c) HARD – FAANG LEVELLED
